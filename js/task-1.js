861058e4808f089df9192ecfb82f9107fe87aff2
DanielaChud <151681916+DanielaChud@users.noreply.github.com>
2024-03-17 20:02:59 +0300

    HM-8

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const itemsEl = document.querySelectorAll(".item");
-console.log(`Number of categories: ${itemsEl.length}`);
-itemsEl.forEach(item => {
-    const categoryName = item.querySelector("h2").textContent;
-    const itemsCount = item.querySelectorAll("li").length;
-    console.log(`Category: ${categoryName}`);
-    console.log(`Elements: ${itemsCount}`);
-});
